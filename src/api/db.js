83f7bcc14843f8a9568171adb3aae2388dc3985f
Ricardo Tan <ricardo.tan.2000@gmail.com>
2019-09-19 18:50:27 +0700

    roll dice animation finished

--- src/api/db.js
+++ src/api/db.js
@@ -2,8 +2,4 @@ import firebase from '../config/firebase'
 
 let db = firebase.firestore();
 const Player = db.collection('Players')
-const Room = db.collection('Rooms')
-export default {
-    Player,
-    Room
-}
+export default Player
