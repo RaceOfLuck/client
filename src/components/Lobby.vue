e470473c1460ebc400a57ea876613bbda1e78f55
maulanaghozii <ghozihz@gmail.com>
2019-09-19 19:04:54 +0700

    add landing page & lobby

---
+++ src/components/Lobby.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="lobby">
+    <h1>Lobby Room</h1>
+    <button style="width : 100px">START</button>
+    <div class="box-card">
+      <div class="card" v-for="(player, index) in players" :key="index">
+        <p>{{ player.name }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      players: [
+        { name: "ghozi" },
+        { name: "Faras" },
+        { name: "Guntoro" },
+        { name: "Ricardo" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.lobby {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.card {
+  width: 250px;
+  height: 50px;
+  margin: 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
