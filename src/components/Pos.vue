9dcd66d5b7af6d7a1b1e6e110e06827310e04bd3
igun369 <guntoro.gyk@gmail.com>
2019-09-19 19:53:13 +0700

    frame track ok

---
+++ src/components/Pos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card m-1" style="border: 1px solid black; width: 2em; height: 30px">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
