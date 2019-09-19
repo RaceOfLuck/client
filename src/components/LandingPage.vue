7ea230ff57e3a9bf5f42eeb7ca3ad8b3cdd87b9e
maulanaghozii <ghozihz@gmail.com>
2019-09-19 19:04:54 +0700

    add landing page & lobby

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cover">
+    <div class="input">
+      <form>
+        <input type="text" placeholder="Input Your Name" />
+        <router-link to="/lobby">
+          <input type="submit" value="MASUK" />
+        </router-link>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.cover {
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.input {
+  height: 50px;
+  width: 450px;
+}
+
+form {
+  display: flex;
+  justify-content: space-between;
+}
+input {
+  width: 300px;
+  height: 40px;
+  background-color: transparent;
+  border: 3px solid black;
+  padding: 5px;
+}
+input[type="submit"] {
+  width: 100px;
+  height: 40px;
+}
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <LandingPage></LandingPage>
+</template>
+
+<script>
+import LandingPage from "../components/LandingPage";
+export default {
+  components: {
+    LandingPage
+  }
+};
+</script>
+
+<style>
+</style>
