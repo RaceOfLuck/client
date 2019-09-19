2d696ead504f3c9f6f0c7d7c06cbf5480a2435af
Ricardo Tan <ricardo.tan.2000@gmail.com>
2019-09-19 19:10:18 +0700

    modify dice to singledice

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
-        <div>Roll Count left : {{ dice }}</div>
+        <div>Roll Count left : {{ diceCount }}</div>
+        <div>Roll Result : {{ result }}</div>
             <br><br>
         <form>
-            <div><div id = "one"><img src='https://66.media.tumblr.com/4487da1524b8d90e0007c5da72fc49e7/tumblr_puhatt8Lzf1s8erfco3_100.png'></div>
-            <div id = "two"><img src='https://66.media.tumblr.com/fd6e3e52de325f1f2023e320a1ff8191/tumblr_puhatt8Lzf1s8erfco4_100.png'></div></div>
-            <input type="number" min="0" v-model="count">
+            <div>
+                <div id = "one">
+                    <img src='https://66.media.tumblr.com/4487da1524b8d90e0007c5da72fc49e7/tumblr_puhatt8Lzf1s8erfco3_100.png'>
+                </div>
+            </div>
+            <!-- <div id = "two"><img src='https://66.media.tumblr.com/fd6e3e52de325f1f2023e320a1ff8191/tumblr_puhatt8Lzf1s8erfco4_100.png'></div></div> -->
+            <input type="number" min="0" v-model="numberOfRolls">
             <button @click.prevent="roll()">Roll</button>
         </form>
     </div>
@@ -15,18 +20,18 @@
 export default {
     data : function () {
         return {
-            dice : "-",
-            count : 0,
+            diceCount : 0,
+            numberOfRolls : 0,
+            result : 0,
             myVar : null
         }
     },
     methods: {
         roll() {
-            const numberOfRolls = this.count
+            const numberOfRolls = this.numberOfRolls
             this.rollDice( numberOfRolls )
         },
         rollDice(numberOfRolls) {      
-            console.log(`rolling the dice`);
             var diceThrow = Math.floor((Math.random() * 6) + 1);
 
             const dicePics1 = [
@@ -38,21 +43,23 @@ export default {
                 "<img src='https://66.media.tumblr.com/ee1eeff4acbec1af9223c76df28dae8a/tumblr_puhatt8Lzf1s8erfco6_100.png'>"
             ]
 
-            const dicePics2 = [
-                "<img src='https://66.media.tumblr.com/5240dc2418bbd8cadafc6a238e84f51d/tumblr_puhatt8Lzf1s8erfco2_100.png'>",
-                "<img src='https://66.media.tumblr.com/a9e433dace9d745dbd7c157ddacdf6f3/tumblr_puhatt8Lzf1s8erfco1_100.png'>",
-                "<img src='https://66.media.tumblr.com/4487da1524b8d90e0007c5da72fc49e7/tumblr_puhatt8Lzf1s8erfco3_100.png'>",
-                "<img src='https://66.media.tumblr.com/fd6e3e52de325f1f2023e320a1ff8191/tumblr_puhatt8Lzf1s8erfco4_100.png'>",
-                "<img src='https://66.media.tumblr.com/1baf8a8bffc14a37b2ceda91d7ba495c/tumblr_puhatt8Lzf1s8erfco5_100.png'>",
-                "<img src='https://66.media.tumblr.com/ee1eeff4acbec1af9223c76df28dae8a/tumblr_puhatt8Lzf1s8erfco6_100.png'>"
-            ]
+            // const dicePics2 = [
+            //     "<img src='https://66.media.tumblr.com/5240dc2418bbd8cadafc6a238e84f51d/tumblr_puhatt8Lzf1s8erfco2_100.png'>",
+            //     "<img src='https://66.media.tumblr.com/a9e433dace9d745dbd7c157ddacdf6f3/tumblr_puhatt8Lzf1s8erfco1_100.png'>",
+            //     "<img src='https://66.media.tumblr.com/4487da1524b8d90e0007c5da72fc49e7/tumblr_puhatt8Lzf1s8erfco3_100.png'>",
+            //     "<img src='https://66.media.tumblr.com/fd6e3e52de325f1f2023e320a1ff8191/tumblr_puhatt8Lzf1s8erfco4_100.png'>",
+            //     "<img src='https://66.media.tumblr.com/1baf8a8bffc14a37b2ceda91d7ba495c/tumblr_puhatt8Lzf1s8erfco5_100.png'>",
+            //     "<img src='https://66.media.tumblr.com/ee1eeff4acbec1af9223c76df28dae8a/tumblr_puhatt8Lzf1s8erfco6_100.png'>"
+            // ]
             let randomDice = Math.floor(Math.random() * dicePics1.length)
-            let randomDice2 = Math.floor(Math.random() * dicePics1.length)
+            // let randomDice2 = Math.floor(Math.random() * dicePics1.length)
             one.innerHTML = dicePics1[randomDice];
-            two.innerHTML = dicePics2[randomDice2];
+            // two.innerHTML = dicePics2[randomDice2];
 
 
-            this.dice = numberOfRolls
+            this.diceCount = numberOfRolls
+
+            this.result = randomDice + 1
             --numberOfRolls;
             // Check if there is a throw left...
             if (numberOfRolls < 0) {
