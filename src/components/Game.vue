b225d64ff7436734a5c98b31c9f459ddbf048344
igun369 <guntoro.gyk@gmail.com>
2019-09-20 15:07:10 +0700

    finish game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
       <Board></Board>
-      <div class="container">
-        <Track v-for="player in players" :key="player.username" :player="player"/>
+      <!-- <pre> {{ players }} </pre> -->
+
+      <div class="container-fluid">
+        <div class="d-flex align-items-center" v-for="player in players" :key="player.username">
+          <span class="mx-2"> {{ player.username }} </span>
+          <Track :player="player"/>
+        </div>
       </div>
   </div>
 </template>
@@ -10,6 +15,7 @@
 <script>
 import Board from '../components/Board'
 import Track from '../components/Track'
+import room from '../api/db'
 export default {
     components: { Board, Track },
     name : "Game",
@@ -18,16 +24,38 @@ export default {
       return this.$store.state.dice.result
     },
     players() {
+      // return this.$store.state.players
       return this.$store.state.players
     }
   },
   watch: {
     dice(to, from) {
-      console.log(this.dice)
+      // console.log(this.dice)
       // this.players[0].position += this.dice;
-      this.players.forEach(player => {
-        player.position += this.dice
+      this.$store.state.room.players.forEach( player => {
+        if ( player.username == localStorage.getItem('username') ) {
+          console.log( player.username , " <<<<<<<<< " )
+          player.position += this.dice
+          let myroom = this.$store.state.room
+            room.doc( this.$store.state.room.id )
+            .set(myroom)
+            .then(_ => { console.log( " UPDATed")})
+            .catch ( console.log )
+          if ( player.position >= 10) {
+            
+            // this.$swal(`${player.username} is win!!!`);
+            let myroom = this.$store.state.room
+            myroom.isPlaying = false;
+            room.doc( this.$store.state.room.id )
+            .set(myroom)
+
+            this.$router.push('/userRoom')
+
+          }
+        }
       })
+      // this.players.forEach(player => {
+      // })
     }
   },
 }
