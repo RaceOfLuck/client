2198a09bb314f890f2b5666bcf89145e35c38d41
igun369 <guntoro.gyk@gmail.com>
2019-09-20 11:42:28 +0700

    added gameboard;

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-      GAME BROH
+      <Board></Board>
+      <div class="container">
+        <Track v-for="player in players" :key="player.username" :player="player"/>
+      </div>
   </div>
 </template>
 
 <script>
+import Board from '../components/Board'
+import Track from '../components/Track'
 export default {
-    name : "Game"
+    components: { Board, Track },
+    name : "Game",
+    computed: {
+    dice() {
+      return this.$store.state.dice.result
+    },
+    players() {
+      return this.$store.state.players
+    }
+  },
+  watch: {
+    dice(to, from) {
+      console.log(this.dice)
+      // this.players[0].position += this.dice;
+      this.players.forEach(player => {
+        player.position += this.dice
+      })
+    }
+  },
 }
 </script>
 
